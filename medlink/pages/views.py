19666d378749ba694134885a41f2a6a37dd106be
Jamie Zhang <jzhang17@nd.edu>
2021-03-20 16:13:00 -0400

    migrating site from DB to django

---
+++ medlink/pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+# Create your views here.
+
+
+def index(request):
+    try:
+        return render(request, "index.html")
+    except Exception as e:
+        print(e)
+        print("error")
