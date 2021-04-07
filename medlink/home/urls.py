4b2e3a604187a2827a51a75d383035e5770c5406
Jiaming Zhang <jzhang17@nd.edu>
2021-04-07 10:46:43 -0400

    remove unused url pattern

--- medlink/home/urls.py
+++ medlink/home/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('hospital/post_job/', views.hospital_post_job, name='home/hospital/post_job'),
     path(r'profile_update/', views.profile_update, name='profile_update'),    
     path(r'logout/', views.log_out, name='logout'),    
-    path('', views.home, name='home'),
     path(r'job_query/', views.job_query, name='job_query')
 ]
