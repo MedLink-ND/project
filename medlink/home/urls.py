3db766a598a5526d5455f2ca3a38f6fd7630bbfe
Marcelo Bernath Piccolotto <mbernath@nd.edu>
2021-04-25 21:55:22 -0400

    add a link form

--- medlink/home/urls.py
+++ medlink/home/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('hospital/post_job/', views.hospital_post_job, name='home/hospital/post_job'),
     # path('hospital/post_job/', ontactWizard.as_view([JobCreationForm1, JobCreationForm2])),
     url(r'hospital/update/(?P<job_id>\d+)/$', views.hospital_update_job,),    
-    path(r'hospital/profile_update/', views.profile_update, name='profile_update'),    
+    path(r'hospital/profile_update/', views.profile_update, name='profile_update'), 
+    path(r'worker/profile_update/', views.worker_profile_update, name='worker_profile_update'),   
     path(r'logout/', views.log_out, name='logout'),    
     path(r'job_query/', views.job_query, name='job_query'),
 ]
